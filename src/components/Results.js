01cdef8a0360f3354c9b67b8718708611ba2d2a9
Daniel W <danieldwalford@gmail.com>
2020-07-22 09:25:17 +0100

    Added new topics from API

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,17 +3,35 @@ import '../styles/App.css';
 
 export class Results extends React.Component {
 
+
     render() {
         let displayList;
+        let topic;
         if (this.props.results != null && this.props.results[0] != null && this.props.results[0].items != null && this.props.results[0].items.length > 0) {
             displayList = this.props.results[0].items.map(function (singleResult, index) {
+                topic = "";
+                for (let i = 1; i < 10; i++) {
+                    let stringAccessor = `topic${i}`;
+                    if (singleResult[stringAccessor] != null) {
+                        if (i === 1) {
+                            topic += singleResult[stringAccessor]
+                        } else {
+                            topic += ` / ${singleResult[stringAccessor]}`;
+                        }
+                    }
+                }
+                let topicHTML = <span></span>;
+                if (topic.length > 0) {
+                    topicHTML = <span>Topic: {topic}<br/></span>;
+                }
                 return <li key={index} className="col col--md-34 col--lg-50 search-results__item search-result-item">
                     <a href={singleResult.link}>
                         {singleResult.title}
                         <p className="search-results__meta">
+                            {topicHTML}
                             Code: {singleResult.alias}<br/>
                             {singleResult.description}
-                            </p>
+                        </p>
                     </a>
                 </li>;
             });
